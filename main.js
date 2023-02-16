e2370c1936b901da550cf5e0e7f80f8bb6cc0e18
Kennedy <techsit360@gmail.com>
2023-02-16 05:12:09 -0500

    Finished project

---
+++ main.js
@@ -0,0 +1,147 @@
+let myLibrary = [];
+
+/* addBookToLibrary(
+  "A Better Nigeria: A Better World",
+  "Ken Chukwudi",
+  "1960",
+  "Read"
+);
+addBookToLibrary("A Passage to India", "E.M. Foster", "1800", "Not read yet");
+addBookToLibrary("A Revenue Stamp", "Amrita Pritam", "1450", "Not read yet");
+addBookToLibrary("Death of a City", "Amrita Pritam", "1640", "Read"); */
+
+// CONSTRUCTOR
+function Book(title, author, pages, read = "no") {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  /* this.info = function () {
+    return `${title} by ${author}, ${pages}, ${read}`;
+  }; */
+}
+
+// MODAL SECTION
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const openModalBtn = document.querySelector(".btn-open");
+const closeModalBtn = document.querySelector(".btn-close");
+
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+openModalBtn.addEventListener("click", openModal);
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+closeModalBtn.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+let addBook = document.querySelector("#add-book");
+addBook.addEventListener("click", closeModal);
+
+//ADD TO LIBRARY FUNCTION
+function addBookToLibrary() {
+  let title = document.getElementById("title").value;
+  let author = document.getElementById("author").value;
+  let pages = document.getElementById("pages").value;
+  let read = document.getElementById("read").checked;
+  let readStatus = read ? "Read" : "Not read yet";
+  let book = new Book(title, author, pages, readStatus);
+  for (let book of myLibrary) {
+    if (book.title === title) {
+      alert("Book already exists in library!");
+      return;
+    }
+  }
+  if (title.value !== "" && author !== "" && pages !== "") {
+    myLibrary.push(book);
+  }
+  displayBooks();
+}
+
+// DEFAULT ACTION SHOULDNT BE TAKEN AS IT NORMALLY WOULD
+const form = document.querySelector("form");
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+});
+
+// DISPLAY ALL ELEMENT IN AN ARRAY
+function displayBooks() {
+  let booksContainer = document.querySelector("#books-container");
+  booksContainer.innerHTML = "";
+  myLibrary.forEach((element, index) => {
+    const divEl = document.createElement("div");
+    divEl.classList.add("card");
+    divEl.id = `card-${index}`;
+    if (typeof element === "object") {
+      Object.values(element).forEach((value) => {
+        const paraEl = document.createElement("p");
+        paraEl.textContent = value;
+        divEl.appendChild(paraEl);
+        booksContainer.appendChild(divEl);
+      });
+    }
+    // TOGGLE BUTTON FOR READ STATUS
+    let paraToggle = document.querySelector(".card p:nth-child(4)");
+    let readToggle = document.createElement("button");
+
+    readToggle.innerHTML = paraToggle.innerHTML;
+    readToggle.setAttribute("class", "toggle-button");
+    readToggle.setAttribute("type", "button");
+    paraToggle.parentNode.replaceChild(readToggle, paraToggle);
+    if (readToggle.textContent === "Read") {
+      readToggle.style.backgroundColor = "#7FFFD4";
+    } else if (readToggle.textContent === "Not read yet") {
+      readToggle.style.backgroundColor = "#FF5733";
+    }
+
+    readToggle.addEventListener("click", function () {
+      if (readToggle.textContent === "Read") {
+        return (
+          (readToggle.textContent = "Not read yet"),
+          (readToggle.style.backgroundColor = "#FF5733")
+        );
+      } else {
+        return (
+          (readToggle.textContent = "Read"),
+          (readToggle.style.backgroundColor = "#7FFFD4")
+        );
+      }
+    });
+
+    // REMOVE DIV THAT CONTAINS LIBRARY CARD
+    function removeCard(cardIndex) {
+      const card = document.querySelector(`#card-${cardIndex}`);
+      card.remove();
+      //
+      myLibrary.splice(cardIndex, 1);
+    }
+
+    // REMOVE DIV THAT CONTAINS LIBRARY CARD
+    let removeButton = document.createElement("button");
+    removeButton.setAttribute("type", "button");
+    removeButton.classList.add("remove-card");
+    removeButton.textContent = "Remove book";
+    removeButton.style.backgroundColor = "#D54A2D";
+    removeButton.addEventListener("click", () => {
+      removeCard(index);
+    });
+    divEl.appendChild(removeButton);
+  });
+}
+// TRIGGER ADD BOOK BUTTON
+addBook.addEventListener("click", addBookToLibrary);
+//addBook.addEventListener("click", displayBooks);
+
+// RESET FORM INPUT
+
+let resetBtn = document.getElementById("reset");
+let inputs = document.querySelectorAll("input");
+resetBtn.addEventListener("click", () => {
+  inputs.forEach((input) => (input.value = ""));
+});
